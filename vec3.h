26077df173b92faa1e45927518b1d4961a4cc792
Le Marton <martin@brontedisko.de>
2022-01-29 16:34:22 +0100

    Add vec3 utility functions

--- vec3.h
+++ vec3.h
@@ -61,4 +61,46 @@ class vec3 {
 using point3 = vec3;    // 3D point
 using color = vec3;     // RGB color
 
+// vec3 Utility Functions
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+    return vec3(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return v * t;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+    return v * (1 / t);
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
 #endif
