864d9ea691bb17afa98b8c0232db0f9f7d03ae2a
Le Marton <martin@brontedisko.de>
2022-01-29 17:18:16 +0100

    Refactor write color / write single pixel

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 
+#include "color.h"
 #include "vec3.h"
 
 #include <iostream>
@@ -17,16 +18,14 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            double r = double(i) / (image_width-1);
-            double g = double(j) / (image_height-1);
-            double b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(
+                double(i) / (image_width - 1),
+                double(j) / (image_height - 1),
+                0.25
+            );
+            write_color(std::cout, pixel_color);
         }
     }
-}
 
+    std::cerr << "\nDone\n";
+}
